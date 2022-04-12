717d9c78a2aaa07e9c33fa29700b2a9d6d06151b
rye <thuncol12@naver.com>
2022-04-12 17:13:05 +0900

    :zap: 투표글 상세 정보 수정, 결과 시안 추가, 관심태그 게시판 수정

--- src/routes/Post.js
+++ src/routes/Post.js
@@ -5,6 +5,7 @@ import { useHistory } from "react-router-dom";
 import Banner2 from "../components/banner/Banner2";
 import BoardBanner from "../components/board/BoardBanner";
 import Detail from "../components/post/Detail";
+import Result from "../components/post/Result";
 import Comments from "../components/post/Comments";
 import style from "./Post.module.css";
 
@@ -16,24 +17,41 @@ function Post() {
   const history = useHistory();
 
   const [loadingPost, setLoadingPost] = useState(true);
+  const [loadingOptions, setLoadingOptions] = useState(true);
   const [loadingComments, setLoadingComments] = useState(true);
+  const [loadingResult, setLoadingResult] = useState(true);
 
-  const [postData, setPostData] = useState({});
+  const [postData, setPostData] = useState({}); // post
+  const [options, setOptions] = useState({}); // options
   const [comments, setComments] = useState([]);
-  const [visible, setVisible] = useState(true); // 댓글 visible
+  const [visible, setVisible] = useState(false); // 댓글 visible
+  const [likedComments, setLikedComments] = useState([]); // 유저가 좋아요한 댓글들
+  const [result, setResult] = useState([]); // 결과
+
   const [newComment, setNewComment] = useState("");
 
-  const [results, setResults] = useState([]);
-
-  // 처음. 글, 결과, 댓글 불러옴
+  // 글
   const getPost = async () => {
     try {
-      const response = await axios.get(
-        `http://localhost:3000/api/posts/${id}`,
-        { withCredentials: true }
-      );
+      const response = await axios.get(`http://localhost:3000/api/posts/${id}`);
+      // const response = await axios.get(`http://localhost:3000/post`);
       setPostData(response.data);
       setLoadingPost(false);
+      // console.log("post res.d", response.data);
+    } catch (error) {
+      console.log("에러post:: ", error);
+    }
+  };
+  // 내 선택지
+  const getOptions = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:3000/api/posts/${id}/options`,
+        { withCredentials: true }
+      );
+      // const response = await axios.get(`http://localhost:3000/options`);
+      setOptions(response.data);
+      setLoadingOptions(false);
     } catch (error) {
       try {
         const response = await axios.get(
@@ -56,8 +74,7 @@ function Post() {
       }
     }
   };
-
-  // 댓글 불러오기
+  // 댓글
   const getComments = async () => {
     try {
       // 찐
@@ -65,8 +82,10 @@ function Post() {
         `http://localhost:3000/api/posts/${id}/comments`,
         { withCredentials: true }
       );
+      // const response = await axios.get(`http://localhost:3000/ccomments`);
       setComments(response.data.comments);
       setVisible(response.data.isVisible);
+      setLikedComments(response.data.mylikes);
       setLoadingComments(false);
     } catch (error) {
       try {
@@ -90,24 +109,45 @@ function Post() {
       }
     }
   };
+  // 결과
+  const getResult = async () => {
+    try {
+      // 찐
+      const response = await axios.get(
+        `http://localhost:3000/api/posts/${id}/options/results`
+      );
+      // const response = await axios.get(`http://localhost:3000/result`);
+      setResult(response.data);
+      setLoadingResult(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // 처음. 글, 내선택지, 결과, 댓글 불러옴
   useEffect(() => {
     getPost();
+    getOptions();
+    getResult();
     getComments();
   }, []);
+
+  // 내가 좋아요한 댓글인지
+  const compare = (id) => {
+    return likedComments.includes(id);
+  };
 
   // 새 댓글 작성
   const onChange = (event) => {
     setNewComment(event.target.value);
   };
-  const submitHandler = async (event) => {
-    event.preventDefault();
+  const submitHandler = async () => {
     try {
       await axios.post(
         `http://localhost:3000/posts/${id}/comments`,
         { content: newComment },
         { withCredentials: true }
       );
-      getComments();
     } catch (error) {
       try {
         const response = await axios.get(
@@ -137,20 +177,29 @@ function Post() {
       <BoardBanner board_name={false} newPost={false} />
 
       {/* 게시글 상세 조회 */}
-      {loadingPost ? null : (
+      {loadingPost || loadingOptions ? null : (
         <Detail
-          id={id}
+          id={postData.id}
           title={postData.title}
           profilePic={postData.profile}
           content={postData.content}
           registerDate={postData.registerDate}
           finishDate={postData.finishDate}
           author={postData.author}
-          tags={postData.tags} // 배열
-          choice={postData.choice} // 배열
           isFinished={postData.isFinished}
+          tags={postData.tags} // 배열
+          isVoted={options.isVoted}
+          choices={options.choices} // 배열
+          myVote={options.myVote} // 숫자
         />
       )}
+
+      {/* 결과 */}
+      {loadingResult ? null : postData.isFinished ? (
+        <div className={style.container}>
+          <Result result={result} />
+        </div>
+      ) : null}
 
       {/* 댓글 */}
       {visible === false ? null : loadingComments ? null : (
@@ -168,20 +217,37 @@ function Post() {
           </form>
 
           <ul className={style.comments}>
-            {comments.map((comment) => (
-              <Comments
-                postId={id}
-                key={comment.id}
-                id={comment.id}
-                choiceNum={comment.choiceId}
-                status={comment.status}
-                nickname={comment.author}
-                profilePic={comment.profilePic}
-                date={comment.registerDate}
-                likedNum={comment.likes}
-                comment={comment.content}
-              />
-            ))}
+            {comments.map((comment) =>
+              compare(comment.id) ? (
+                <Comments
+                  postId={id}
+                  key={comment.id}
+                  id={comment.id}
+                  choiceNum={comment.choiceId}
+                  status={comment.status}
+                  nickname={comment.author}
+                  profilePic={comment.profilePic}
+                  date={comment.registerDate}
+                  likedNum={comment.likes}
+                  comment={comment.content}
+                  mylike={true}
+                />
+              ) : (
+                <Comments
+                  postId={id}
+                  key={comment.id}
+                  id={comment.id}
+                  choiceNum={comment.choiceId}
+                  status={comment.status}
+                  nickname={comment.author}
+                  profilePic={comment.profilePic}
+                  date={comment.registerDate}
+                  likedNum={comment.likes}
+                  comment={comment.content}
+                  mylike={false}
+                />
+              )
+            )}
           </ul>
         </div>
       )}
